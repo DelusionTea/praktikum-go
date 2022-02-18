b348ebfc4a251b63288573a7c8bfd3c8df73ddd4
KindOfTea <delusionkind@gmail.com>
2022-02-19 02:42:58 +0300

    remove bugs

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	//http.HandleFunc("/", myrequest)
 	router := httprouter.New()
-	router.POST("/", Shorting.HandlerCreateShortURL)
-	router.GET("/:id", Shorting.HandlerGetURLByID)
+	router.POST("/", HandlerCreateShortURL)
+	router.GET("/:id", HandlerGetURLByID)
 	//Сервер должен быть доступен по адресу: http://localhost:8080.
 	//http.ListenAndServe(":8080", nil)
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
