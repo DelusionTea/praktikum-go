d421f0e92f26d90983ef9b1874a250415603e6de
KindOfTea <delusionkind@gmail.com>
2022-02-19 02:50:43 +0300

    add handlers path

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	//http.HandleFunc("/", myrequest)
 	router := httprouter.New()
-	router.POST("/", HandlerCreateShortURL)
-	router.GET("/:id", HandlerGetURLByID)
+	router.POST("/", handlers.HandlerCreateShortURL)
+	router.GET("/:id", handlers.HandlerGetURLByID)
 	//Сервер должен быть доступен по адресу: http://localhost:8080.
 	//http.ListenAndServe(":8080", nil)
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
