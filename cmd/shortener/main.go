f603cbcd87288413a5f9899c0f498ae02c974550
KindOfTea <delusionkind@gmail.com>
2022-02-18 21:27:04 +0300

    edit main file 1 incr

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+    "net/http"
+	"github.com/julienschmidt/httprouter"
+	"github.com/DelusionTea/praktikum-go/blob/main/internal/app/handlers/"
+)
+
+func main() {
+	//http.HandleFunc("/", myrequest)
+	router := httprouter.New()
+	router.POST("/", HandlerCreateShortURL)
+	router.GET("/:id", HandlerGetURLByID)
+	//Сервер должен быть доступен по адресу: http://localhost:8080.
+	//http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
