448a41361fe43087e8648662a9fdf9779e53300b
KindOfTea <delusionkind@gmail.com>
2022-02-19 02:24:01 +0300

    folder settings

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,13 +5,14 @@ import (
 	"github.com/DelusionTea/praktikum-go/internal/app/handlers"
 	"log"
 	"net/http"
+	Shorting "../../internal/app/handlers/urlshorter.go"
 )
 
 func main() {
 	//http.HandleFunc("/", myrequest)
 	router := httprouter.New()
-	router.POST("/", HandlerCreateShortURL)
-	router.GET("/:id", HandlerGetURLByID)
+	router.POST("/", Shorting.HandlerCreateShortURL)
+	router.GET("/:id", Shorting.HandlerGetURLByID)
 	//Сервер должен быть доступен по адресу: http://localhost:8080.
 	//http.ListenAndServe(":8080", nil)
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
