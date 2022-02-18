cd759d2df9f75a697807ad580f15b4f8c418e771
KindOfTea <delusionkind@gmail.com>
2022-02-19 01:48:31 +0300

    edit github path

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
-	"github.com/Delusionkind/internal/app/handlers"
+	"github.com/DelusionTea/praktikum-go/internal/app/handlers"
 	"log"
 	"net/http"
 )
