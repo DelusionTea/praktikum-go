f965f9d01ab6ba74f746569f28022bb1796f8a50
DelusionTea <DelusionKind@gmail.com>
2022-03-11 08:32:34 -0800

    new handlers edit main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"context"
 	"github.com/DelusionTea/praktikum-go/cmd/conf"
 	"github.com/DelusionTea/praktikum-go/internal/app/handlers"
-	"github.com/go-chi/chi/v5"
+	"github.com/DelusionTea/praktikum-go/internal/memory"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 )
 
+func setupRouter(repo memory.MemoryInterface, baseURL string) *gin.Engine {
+	router := gin.Default()
+
+	handler := handlers.New(repo, baseURL)
+
+	router.GET("/:id", handler.HandlerGetURLByID)
+	router.POST("/", handler.HandlerCreateShortURL)
+	router.POST("/api/shorten", handler.HandlerShortenURL)
+
+	router.HandleMethodNotAllowed = true
+
+	return router
+}
+
 func main() {
+	cfg := conf.GetConfig()
 
-	router := chi.NewRouter()
-	log.Println(router)
-	config := conf.GetConfig()
+	handler := setupRouter(memory.NewMemoryFile(cfg.FilePath), cfg.BaseURL)
 
-	handler := handlers.NewHandler(config)
-	log.Println(handler)
-	handler.CallHandlers(router)
-	log.Fatal(http.ListenAndServe("localhost:8080", router))
+	server := &http.Server{
+		Addr:    conf.ServerAddress,
+		Handler: handler,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		log.Fatal(server.ListenAndServe())
+		cancel()
+	}()
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+	select {
+	case <-sigint:
+		cancel()
+	case <-ctx.Done():
+	}
+	server.Shutdown(context.Background())
 }
